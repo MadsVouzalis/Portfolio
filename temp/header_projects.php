10524c64f109a41aab24ecef6b238ecc37e04a2d
Mads Kristensen <madsvouzalis@icloud.com>
2016-09-09 11:43:36 +0200

    Live version

---
+++ temp/header_projects.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+
+<!-- Meta Data -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Developed By Vouzalis Design">
+<meta name="author" content="Vouzalis Design">
+<meta name="keywords" content="Marketing automation, content quality websites, webdesign 2016, wireframing, Landingpage">
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="images/favicon.ico">
+
+<title>Folio - Small Creative Portfolio</title>
+
+
+
+<!-- Stylesheets -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/font-awesome.css">
+<link rel="stylesheet" href="css/owl.theme.css">
+<link rel="stylesheet" href="css/owl.carousel.css">
+<link rel="stylesheet" href="css/supersized.css">
+<link rel="stylesheet" href="css/nivo-theme.css">
+<link rel="stylesheet" href="css/nivo-lightbox.css">
+
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/responsive.css">
+<!-- Theme Color Stylesheet -->
+<link rel="stylesheet" href="css/theme_color.css">
+
+<!-- Google Font -->
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700' rel='stylesheet' type='text/css'>
+
+<!-- Bootstrap core CSS -->
+
+<link href="css/theme.css" rel="stylesheet">
+<link href="css/bootstrap-reset.css" rel="stylesheet">
+
+<!--external css-->
+<link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="css/mixitup.css">
+<link href="css/projects-style.css" rel="stylesheet">
+
+
+<!-- Bootstrap core CSS -->
+    <link href="temp/css/bootstrap.min.css" rel="stylesheet">
+    <link href="temp/css/theme.css" rel="stylesheet">
+    <link href="temp/css/bootstrap-reset.css" rel="stylesheet">
+    
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="temp/css/mixitup.css">
+    <link href="temp/css/style.css" rel="stylesheet">
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+
+
+
+
+
+
+
+ 
+
+
